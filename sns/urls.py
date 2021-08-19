d83ffc2ef78d2db6687d34c4ac605e1e5efee64a
ysnr8811 <y.f.arctos9@gmail.com>
2021-08-19 22:47:34 +0900

    サーバーの起動

--- sns/urls.py
+++ sns/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:page>', views.index, name='index'),
-    path('groups', views.groups, name='groups'),
-    path('add', views.add, name='add'),
-    path('creategroup', views.creategroup, name='creategroup'),
-    path('post', views.post, name=' post'),
-    path('share/<int:share_id>', views.share, name='share'),
-    path('good/<int:good_id>', views.good, name='good'),
+    # 未完成なのでこの部分をコメントアウトしないと動かない
+    # path('', views.index, name='index'),
+    # path('<int:page>', views.index, name='index'),
+    # path('groups', views.groups, name='groups'),
+    # path('add', views.add, name='add'),
+    # path('creategroup', views.creategroup, name='creategroup'),
+    # path('post', views.post, name=' post'),
+    # path('share/<int:share_id>', views.share, name='share'),
+    # path('good/<int:good_id>', views.good, name='good'),
 ]
